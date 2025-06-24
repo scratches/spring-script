9dd19f2d6e7658a560f71c60ececa019646cf8ed
Dave Syer <david.syer@broadcom.com>
2025-06-24 06:06:35 +0100

    Initial revision script runner

---
+++ src/main/java/org/springframework/script/GenericApplication.java
@@ -0,0 +1,11 @@
+package org.springframework.script;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GenericApplication.class, args);
+	}
+}
